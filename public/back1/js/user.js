19cc9902b3855ca56e59e868a0364135400a14ca
Owen2008 <1273820615@qq.com>
2018-09-28 14:45:38 +0800

    编号问题解决

--- public/back1/js/user.js
+++ public/back1/js/user.js
@@ -1,16 +1,35 @@
 $(function(){
-  $.ajax({
-    type:"get",
-    url:"/user/queryUser",
-    data:{
-      page:1,
-      pageSize:4
-    },
-    dataType:"json",
-    success:function(info){
-    console.log(info)
-    $("tbody").html(template("tmp",info))
-    
+  var currentPage=1;
+  var pageSize=5;
+  function render(){
+    $.ajax({
+      type:"get",
+      url:"/user/queryUser",
+      data:{
+        page:currentPage,
+        pageSize:pageSize
+      },
+      dataType:"json",
+      success:function(info){
+      console.log(info)
+      $("tbody").html(template("tmp",info))
+      
+      }
+    })
+  }
+ render()
+  $("#paginator").bootstrapPaginator({
+    bootstrapMajorVersion:3,
+    currentPage:1,
+    totalPages:10,
+    size:"small",
+    onPageClicked:function(event, originalEvent, type,page){
+      //为按钮绑定点击事件 page:当前点击的按钮值
+      
+      currentPage=page;
+      console.log(currentPage);
+      render()
     }
+  
   })
 })
