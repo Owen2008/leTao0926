8f016c3166cd67b00cdec90b2f30590da6cfcbbb
Owen2008 <1273820615@qq.com>
2018-09-29 15:35:30 +0800

    分页bug

--- public/back1/js/user.js
+++ public/back1/js/user.js
@@ -1,3 +1,4 @@
+// 页面渲染和分页
 $(function(){
   var currentPage=1;
   var pageSize=5;
@@ -12,24 +13,27 @@ $(function(){
       dataType:"json",
       success:function(info){
       console.log(info)
+   
+      $("#paginator").bootstrapPaginator({
+        bootstrapMajorVersion:3,
+        currentPage:currentPage,
+        totalPages:Math.ceil(info.total/info.size),
+        size:"small",
+        onPageClicked:function(event, originalEvent, type,page){
+          //为按钮绑定点击事件 page:当前点击的按钮值
+          
+          currentPage=page;
+          console.log(currentPage);
+          render()
+        }
+      
+      })
       $("tbody").html(template("tmp",info))
       
       }
     })
   }
  render()
-  $("#paginator").bootstrapPaginator({
-    bootstrapMajorVersion:3,
-    currentPage:1,
-    totalPages:10,
-    size:"small",
-    onPageClicked:function(event, originalEvent, type,page){
-      //为按钮绑定点击事件 page:当前点击的按钮值
-      
-      currentPage=page;
-      console.log(currentPage);
-      render()
-    }
-  
-  })
+
 })
+// 禁用按钮功能实现
