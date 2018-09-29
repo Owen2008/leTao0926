d9da31856f53b1578caf6bb168f8e70ad142d5b1
Owen2008 <1273820615@qq.com>
2018-09-29 20:34:05 +0800

    first添加功能完成

--- public/back1/js/first.js
+++ public/back1/js/first.js
@@ -1,7 +1,7 @@
 $(function(){
   // 获取数据动态渲染
   var currentPage=1;
-  var pageSize=2;
+  var pageSize=5;
   function render(){
     $.ajax({
       url:"/category/queryTopCategoryPaging",
@@ -23,7 +23,7 @@ $(function(){
           //为按钮绑定点击事件 page:当前点击的按钮值
           
           currentPage=page;
-          console.log(currentPage);
+        
           render()
         }
       })//分页结束
@@ -32,5 +32,46 @@ $(function(){
     }) //ajax结束
   }//函数完成
   render();
+  //添加功能
+  $(".addbtn").click(function(){
+   $("#addCate").modal("show");
+   //表单验证
+   $("#addfirst").bootstrapValidator({
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields:{
+      categoryName:{
+        validators:{
+          notEmpty:{
+            message:"请输入一级分类名称"
+          }
+        }
+      }
+    }
+   });//非空验证完成
   
+  
+
+  })//addbtn 点击事件结束
+  $("#addfirst").on("success.form.bv",function(e){
+    e.preventDefault();
+    //  数据发给后台,从新渲染页面,表单重置
+   $.ajax({
+     url:"/category/addTopCategory",
+     dataType:"json",
+     data:$("#addfirst").serialize(),
+     type:"post",
+     success:function(info){
+       if(info.success){
+         $("#addCate").modal("hide");
+         currentPage=1;
+         render();
+         $("#addfirst").data("bootstrapValidator").resetForm(true)
+       }
+     }
+   })//ajax
+  })//验证完成的函数
 })
